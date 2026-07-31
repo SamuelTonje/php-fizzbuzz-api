843a9ae4e44c6a62d8ab52eed603a60a7b79eec0
stonje <sam.tonje@gmail.com>
2026-07-31 06:06:20 +0200

    fix: use correct exception on resolver

--- src/Infrastructure/Symfony/Resolver/RequestBodyValueResolver.php
+++ src/Infrastructure/Symfony/Resolver/RequestBodyValueResolver.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Infrastructure\Symfony\Resolver;
 
 use App\Infrastructure\Symfony\Attribute\MapRequestBody;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
